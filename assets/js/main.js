3441f8694f55e6f9eb344290d1a0cc3c8cf10f91
Math <108838380+Math-Rib@users.noreply.github.com>
2025-10-17 14:56:27 -0300

    Interações do Modo Noturno

--- assets/js/main.js
+++ assets/js/main.js
@@ -2,6 +2,8 @@
 
 const btnAncora = document.getElementById('btn-ancora'); // Armazenando o elemento do botão pelo id
 const iconTopo = document.getElementById('icon-ancora'); // Armazenando o elemento da imagem do botão pelo id
+// const btnTema = document.getElementById('icon-dark-mode'); // Armazenando o elemento do modo noturno/claro pelo id
+// const iconTema = document.getElementById('ckeckbox-dark-mode'); // Armazenando o elemento da imagem do modo noturno/claro pelo id
 const perfil = document.querySelector('.perfil'); // Armazenando a classe o seletor pela classe
 const menu = document.querySelector('.menu-dropdown');  // Armazenando o seletor do menu de dropdown pela classe
 
@@ -20,8 +22,11 @@ perfil.addEventListener('click', () =>{  // Criando uma função direcional para
 
 /* Caminhos das Imagens */
 
-const iconePadrao = './assets/img/icones/icone_ancora_padrao.png'; // Variável armazenando o caminho da imagem padrão
-const iconeHover = './assets/img/icones/icone_ancora_hover.png'; // Variável armazenando o caminho da imagem ao passar o mouser por cima
+const iconeAncoraPadrao = './assets/img/icones/icone_ancora_padrao.png'; // Variável armazenando o caminho da imagem da âncora padrão
+const iconeAncoraHover = './assets/img/icones/icone_ancora_hover.png'; // Variável armazenando o caminho da imagem da âncora ao passar o mouser por cima
+// const iconeModoNoturno = './assets/img/icones/icone_modo_noturno.png' // Variável armazenando o caminho da imagem modo noturno
+// const iconeModoClaro = './assets/img/icones/icone_modo_claro.png' // Variável armazenando o caminho da imagem modo claro
+
 
 /* Efeitos do Botão */
 
@@ -48,9 +53,9 @@ window.addEventListener('scroll', () =>{ // Criando uma função direcional atra
 /* Trocando os Icones */
 
 btnAncora.addEventListener('mouseenter', () =>{ // Criando uma função direcional através do evento passar o mouse em cima(hover)
-    iconTopo.src = iconeHover; // Troca o caminho da imagem para a imagem hover
+    iconTopo.src = iconeAncoraHover; // Troca o caminho da imagem para a imagem hover
 });
 
 btnAncora.addEventListener('mouseleave', () =>{ // Criando uma função direcional através do evento quando o mouse sai de cima do botão
-    iconTopo.src = iconePadrao; //Troca o caminho da imagem para a imagem padrão
+    iconTopo.src = iconeAncoraPadrao; //Troca o caminho da imagem para a imagem padrão
 });
